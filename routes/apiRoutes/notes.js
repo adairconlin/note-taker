ae567430708ecca37edeadea1a8356700f1d47aa
adairconlin <adairconlin@gmail.com>
2021-12-15 14:52:10 -0500

    Setting up routes

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const notes = require("../../db/db.json");
+
+router.get("/notes", (req, res) => {
+    
+    
+});
+
+router.post("/notes", (req, res) => {
+    
+})
+
+module.exports = router;
