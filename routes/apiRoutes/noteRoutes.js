c83933535833cc0e7cd838932b65127d6c0576ec
adairconlin <adairconlin@gmail.com>
2021-12-15 15:00:24 -0500

    Connect routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const notes = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
-    
+    console.log(notes);
     
 });
 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const router = require("router");
+const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
